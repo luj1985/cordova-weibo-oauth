36f0b91a80ce5bdaf5914edd5aee8059a165bf18
Lu, Jun <luj1985@gmail.com>
2015-01-04 00:42:46 +0800

    pass arguments check

--- www/weibo-connect.js
+++ www/weibo-connect.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 var weiboPlugin = {
   login : function(success, error) {
-    exec(success, error, "WeiboConnect", "login");
+    exec(success, error, "WeiboConnect", "login", []);
   }
 }
 module.exports = weiboPlugin;
